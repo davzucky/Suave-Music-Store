d13ec7d03a2f135e09f4de7cdcfd7c0e01c5d9bf
David Zucker <davzucky@hotmail.com>
2016-11-21 12:54:30 +0800

    Final commit end of View section

--- src/Music.Store/view.fs
+++ src/Music.Store/view.fs
@@ -4,18 +4,46 @@ open Suave.Html
 
 let divId id = divAttr ["id", id]
 let h1 xml = tag "h1" [] xml
+let h2 s = tag "h2" [] (text s)
 let aHref href = tag "a" ["href", href]
+let cssLink href = linkAttr [ "href", href; " rel", "stylesheet"; " type", "text/css" ]
+let ul xml = tag "ul" [] (flatten xml)
+let li = tag "li" []
 
-let index = 
+let home = [
+    h2 "Home"
+]
+
+let store genres = [
+    h2 "Browse Genres"
+    p [
+        text (sprintf "Select from %d genres:" (List.length genres))
+    ]
+    ul [
+        for g in genres -> 
+            li (aHref (Path.Store.browse |> Path.withParam (Path.Store.browseKey, g)) (text g))
+    ]
+]
+
+let browse genre = [
+    h2 (sprintf "Genre: %s" genre)
+]
+let details id = [
+    h2 (sprintf "Details %d" id)
+]
+
+let index container = 
     html [
         head [
             title "Suave Music Store"
+            cssLink "/Site.css"
         ]
-
         body [
             divId "header" [
                 h1 (aHref Path.home (text "F# Suave Music Store"))
             ]
+
+            divId "main" container
 
             divId "footer" [
                 text "built with "
